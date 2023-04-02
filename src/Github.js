3ab42e34622e545aadd4146915f55308d2a15b4f
DESKTOP-D6TBG7I\gng <irtazazulfiaar143@gmail.com>
2023-04-03 00:53:33 +0500

    completed the whole code with javascript and also had added some css

--- src/Github.js
+++ src/Github.js
@@ -4,12 +4,44 @@ function Github() {
   const [users, setUsers] = useState([]);
   const [inputstring,setinput]=useState("");
   const [count,setcount]=useState("0");
-  const styles={}
-  const inputstyle={}
+  const styles={
+      color: 'red',
+      fontSize: '20px',
+      padding: '10px',
+      border: '2px solid grey',
+      width:'50%'
+    }
+  const inputstyle=
+  {
+    color: 'red',
+    fontSize: '20px',
+    borderRadius: '5px',
+    border: '2px solid grey',
+   
+
+  }
+  const stylesfh=
+  {
+    color: 'black',
+    fontSize: '50px',
+    borderRadius: '5px',
+  }
+  const divstyle={
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflowY: 'auto',
+    padding: '20px',
+    backgroundColor: '#67578e',
+  }
 
   const handleChange=()=>{
+    const inputValue = document.getElementById('infield').value;
+    setinput(inputValue);
 
-    
   }
 
   useEffect(()=>{
@@ -26,10 +58,15 @@ function Github() {
 
 } , [inputstring]);
 return (
-  <div>
-    <h2>GitHub Users</h2>
-    Enter User Name: <input type="text" id='infield' style={inputstyle}  placeholder='Username...'/><br /><br />
-    <button id='button1' onClick={handleChange}>Search</button>
+  <div style={divstyle}>
+    <h2 style={stylesfh}>GitHub Users</h2>
+    
+    <div>
+        Enter User Name: <input type="text" id='infield' style={inputstyle}  placeholder='Username...'/> 
+       <></> <button id='button1' onClick={handleChange}>Search</button>
+      <br /><br />
+    </div>
+    
     {
       users.map(user => (
         <div style={styles}>
